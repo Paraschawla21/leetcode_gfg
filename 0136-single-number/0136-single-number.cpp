31cfa9cc6209eaa6db4971806f2d3c954bb97dcd
PARAS CHAWLA <74090525+Paraschawla21@users.noreply.github.com>
2023-06-05 18:06:05 +0530

    Time: 24 ms (49.41%), Space: 16.9 MB (39.31%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int, int> mp;
+        int ans = 0;
         for (auto i : nums)
         {
-            mp[i]++;
+            ans ^= i;
         }
-        for (auto i : mp)
-        {
-            if (i.second == 1) return i.first;
-        }
-        return -1;
+        return ans;
     }
 };
