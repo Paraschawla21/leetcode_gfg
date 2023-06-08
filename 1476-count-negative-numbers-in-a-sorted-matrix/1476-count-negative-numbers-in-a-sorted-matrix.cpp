489ccfd0e34dc57d968274615aae7970bb5e9b00
PARAS CHAWLA <74090525+Paraschawla21@users.noreply.github.com>
2023-06-08 11:40:18 +0530

    Time: 23 ms (11.48%), Space: 10.4 MB (31.99%) - LeetHub

--- 1476-count-negative-numbers-in-a-sorted-matrix/1476-count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1476-count-negative-numbers-in-a-sorted-matrix/1476-count-negative-numbers-in-a-sorted-matrix.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& grid, vector<vector<bool>> &visited, int i, int j, int &count)
-    {
-        visited[i][j] = true;
-        if (i >= grid.size() || j >= grid[0].size()) return;
-        if (visited[i][j] || grid[i][j] >= 0) return;
-        count++;
-    }
     int countNegatives(vector<vector<int>>& grid) {
-        int r = grid.size();
-        int c = grid[0].size();
-        vector<vector<bool>> visited(r, vector<bool>(c, false));
         int count = 0;
-        for (int i = 0; i < r; i++)
+        int rows = grid.size();
+        int i = 0;
+        int j = grid[0].size() - 1;
+        while (i < rows && j >= 0)
         {
-            for (int j = 0; j < c; j++)
+            if (grid[i][j] < 0)
             {
-                if (!visited[i][j] && grid[i][j] < 0)
-                {
-                    count++;
-                    dfs(grid, visited, i, j, count);
-                }
+                count += rows - i;
+                j--;
             }
+            else i++;
         }
         return count;
     }
