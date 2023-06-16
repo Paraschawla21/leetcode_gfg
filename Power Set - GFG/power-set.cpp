cfa99b7a6c8774c56411cb144d54695c5e3229af
PARAS CHAWLA <74090525+Paraschawla21@users.noreply.github.com>
2023-06-16 18:46:43 +0530

    Added solution - LeetHub

--- Power Set - GFG/power-set.cpp
+++ Power Set - GFG/power-set.cpp
@@ -5,24 +5,40 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
 	public:
+    	void solve(string &s, int n, vector<string>& ans, string &temp, int index)
+    	{
+    	    if (index == n)
+    	    {
+    	        if (temp.size()) ans.push_back(temp);
+    	        return;
+    	    }
+    	    solve(s, n, ans, temp, index + 1);
+    	    
+    	    temp.push_back(s[index]);
+    	    solve(s, n, ans, temp, index + 1);
+    	    temp.pop_back();
+    	    
+    	}
 		vector<string> AllPossibleStrings(string s){
 		    // Code here
 		    int n= s.size();
 		    vector<string> ans;
-		    for (int num = 0; num <= (1 << n) - 1; num++)
-		    {
-		        string temp = "";
-		        for (int i = 0; i < n; i++)
-		        {
-		            if (num & (1 << i))
-		            {
-		                temp += s[i];
-		            }
+		  //  for (int num = 0; num <= (1 << n) - 1; num++)
+		  //  {
+		  //      string temp = "";
+		  //      for (int i = 0; i < n; i++)
+		  //      {
+		  //          if (num & (1 << i))
+		  //          {
+		  //              temp += s[i];
+		  //          }
 		            
-		        }
-		        if (temp.size() > 0)
-		        ans.push_back(temp);
-		    }
+		  //      }
+		  //      if (temp.size() > 0)
+		  //      ans.push_back(temp);
+		  //  }
+		    string temp = "";
+		    solve(s, n, ans, temp, 0);
 		    sort(ans.begin(), ans.end());
 		    return ans;
 		}
