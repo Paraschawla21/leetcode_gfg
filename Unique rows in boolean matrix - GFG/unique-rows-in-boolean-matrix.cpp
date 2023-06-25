64641f7cd6bad99474599364bb80cfce77438abe
PARAS CHAWLA <74090525+Paraschawla21@users.noreply.github.com>
2023-06-26 00:41:17 +0530

    Added solution - LeetHub

---
+++ Unique rows in boolean matrix - GFG/unique-rows-in-boolean-matrix.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX 1000
+
+vector<vector<int>> uniqueRow(int M[MAX][MAX],int row,int col);
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    #define MAX 1000
+    vector<vector<int>> uniqueRow(int M[MAX][MAX],int row,int col)
+    {
+        //Your code here
+        vector<vector<int>> ans;
+        unordered_set<string> st;
+        for (int i = 0; i < row; i++)
+        {
+            string temp = "";
+            for (int j = 0; j < col; j++)
+            {
+                temp += to_string(M[i][j]);
+            }
+            if (st.count(temp) == 0)
+            {
+                st.insert(temp);
+                vector<int> v;
+                for (int k = 0; k < temp.size(); k++)
+                {
+                    v.push_back(temp[k] - '0');
+                }
+                ans.push_back(v);
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+    	int n,m;
+    	cin>>n>>m;
+    	int a[MAX][MAX];
+    	for(int i=0;i<n;i++)
+    	for(int j=0;j<m;j++)
+    	cin>>a[i][j];
+    	Solution ob;
+    	vector<vector<int>> vec = ob.uniqueRow(a,n,m);
+    	for(int i = 0;i<vec.size();i++){
+    	    for(int x : vec[i]){
+    	        cout<<x<<" ";
+    	    }
+    	    cout<<"$";
+    	}
+    	cout<<endl;
+    }
+}
+
+// } Driver Code Ends
