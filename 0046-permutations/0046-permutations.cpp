6f98a5f4a79b6868000f5627fc7abe7795a9a2ac
PARAS CHAWLA <74090525+Paraschawla21@users.noreply.github.com>
2023-08-02 10:22:53 +0530

    Time: 3 ms (67.29%), Space: 7.9 MB (39.26%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private:
+    void solve(vector<int> nums, int index, vector<vector<int>> &ans)
+    {
+        // base case
+        if (index >= nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+
+        // recursion
+        for (int i = index; i < nums.size(); i++)
+        {
+            swap(nums[i], nums[index]);
+            solve(nums, index + 1, ans);
+
+            // backtrack
+            swap(nums[i], nums[index]);
+        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int index = 0;
+        solve(nums, index, ans);
+        return ans;
+    }
+};
