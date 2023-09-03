97820b9ce0544bd6cb54e156cf5fcf2b6801f368
PARAS CHAWLA <74090525+Paraschawla21@users.noreply.github.com>
2023-09-03 11:00:52 +0530

    Time: 0 ms (100%), Space: 6.5 MB (54.47%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -5,20 +5,12 @@ public:
         if (i == m-1 and j == n-1) return 1;
         if (i >= m or j >= n) return 0;
         if (dp[i][j] != -1) return dp[i][j];
-        return dp[i][j] = solve(i+1, j, m, n, dp) + solve(i, j+1, m, n, dp);
+        int down = solve(i+1, j, m, n, dp);
+        int right = solve(i, j+1, m, n, dp);
+        return dp[i][j] = down + right;
     }
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
-        // return solve(0, 0, m, n, dp);
-        
-        // google's interview solution
-        int N = n + m - 2;
-        int r = m - 1;
-        double ans = 1;
-        for (int i = 1; i <= r; i++)
-        {
-            ans = ans * (N - r + i) / i;
-        }
-        return ans;
+        return solve(0, 0, m, n, dp);
     }
 };
