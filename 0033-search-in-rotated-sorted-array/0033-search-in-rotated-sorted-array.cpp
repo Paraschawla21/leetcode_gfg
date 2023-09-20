6a22a91b0b34b93a6cb80eb40528a54c6f96484f
PARAS CHAWLA <74090525+Paraschawla21@users.noreply.github.com>
2023-09-20 12:42:17 +0530

    Time: 7 ms (19.75%), Space: 11.3 MB (7.06%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int low = 0;
-        int high = nums.size() - 1;
-        while (low < high)
+        int n = nums.size();
+        int l = 0;
+        int en = n-1;
+        while (l < en)
         {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] > nums[high]) low = mid + 1;
-            else high = mid;
+            int mid = l + (en - l) / 2;
+            if(nums[mid] == target) return mid;
+            if (nums[mid] > nums[en]) l = mid + 1;
+            else en = mid;
         }
-        int pivot = low;
-        low = 0;
-        high = pivot - 1;
-        while (low <= high)
+        cout << l;
+        int pivot = l;
+        l = 0;
+        en = pivot - 1;
+        while (l <= en)
         {
-            int mid = low + (high - low) / 2;
+            int mid = l + (en - l) / 2;
             if (nums[mid] == target) return mid;
-            else if (nums[mid] > target) high = mid - 1;
-            else low = mid + 1; 
+            else if (nums[mid] > target) en = mid - 1;
+            else l = mid + 1;
         }
-        low = pivot;
-        high = nums.size() - 1;
-        while (low <= high)
+        l = pivot;
+        en = n - 1;
+        while (l <= en)
         {
-            int mid = low + (high - low) / 2;
+            int mid = l + (en - l) / 2;
             if (nums[mid] == target) return mid;
-            else if (nums[mid] > target) high = mid - 1;
-            else low = mid + 1;
+            else if (nums[mid] > target) en = mid - 1;
+            else l = mid + 1;
         }
         return -1;
     }
